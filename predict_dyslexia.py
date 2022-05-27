6667779680cd940562a38d3b4ef919b53ba43278
MRifqiFz <mrifqi389@gmail.com>
2022-05-27 17:54:08 +0700

    Create code to predict input

--- predict_dyslexia.py
+++ predict_dyslexia.py
@@ -6,11 +6,13 @@ import imutils
 import cv2
 # construct the argument parser and parse the arguments
 # load the handwriting OCR model
+IMAGE_WIDTH = 150
+IMAGE_HEIGHT = 150
 print("[INFO] loading handwriting OCR model...")
-model = load_model('./Model/az_handwritten/1/az_model.h5')
+model = load_model('./Model/dyslexia/1/dyslexia_model.h5')
 # load the input image from disk, convert it to grayscale, and blur
 # it to reduce noise
-image = cv2.imread('Data/Testing/BUKU.jpg')
+image = cv2.imread('./saku.png')
 image = imutils.resize(image, width=300)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -39,21 +41,21 @@ for c in cnts:
 		# if the width is greater than the height, resize along the
 		# width dimension
 		if tW > tH:
-			thresh = imutils.resize(thresh, width=28)
+			thresh = imutils.resize(thresh, width=IMAGE_WIDTH)
 		# otherwise, resize along the height
 		else:
-			thresh = imutils.resize(thresh, height=28)
+			thresh = imutils.resize(thresh, height=IMAGE_HEIGHT)
 		# re-grab the image dimensions (now that its been resized)
 		# and then determine how much we need to pad the width and
 		# height such that our image will be 32x32
 		(tH, tW) = thresh.shape
-		dX = int(max(0, 28 - tW) / 2.0)
-		dY = int(max(0, 28 - tH) / 2.0)
+		dX = int(max(0, IMAGE_WIDTH - tW) / 2.0)
+		dY = int(max(0, IMAGE_HEIGHT - tH) / 2.0)
 		# pad the image and force 32x32 dimensions
 		padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
 			left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
 			value=(0, 0, 0))
-		padded = cv2.resize(padded, (28, 28))
+		padded = cv2.resize(padded, (IMAGE_WIDTH, IMAGE_HEIGHT))
 		# prepare the padded image for classification via our
 		# handwriting OCR model
 		padded = padded.astype("float32") / 255.0
@@ -63,32 +65,34 @@ for c in cnts:
 
 # extract the bounding box locations and padded characters
 boxes = [b[1] for b in chars]
-chars = np.array([c[0] for c in chars], dtype="float32")
+chars = np.array([cv2.cvtColor(c[0], cv2.COLOR_BGR2RGB) for c in chars], dtype="float32")
+
 # OCR the characters using our handwriting recognition model
 preds = model.predict(chars)
-# define the list of label names
-labelNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-labelNames = [l for l in labelNames]
+# # define the list of label names
+# # labelNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# # labelNames = [l for l in labelNames]
+labelNames = ['Corrected','Normal','Reversal']
 
-result = ''
-for pred in preds:
-	i = np.argmax(pred)
-	prob = pred[i]
-	label = labelNames[i]
-	result += label
-print('result: {}'.format(result))
-# loop over the predictions and bounding box locations together
-# for (pred, (x, y, w, h)) in zip(preds, boxes):
-# 	# find the index of the label with the largest corresponding
-# 	# probability, then extract the probability and label
+# result = ''
+# for pred in preds:
 # 	i = np.argmax(pred)
 # 	prob = pred[i]
 # 	label = labelNames[i]
-# 	# draw the prediction on the image
-# 	print("[INFO] {} - {:.2f}%".format(label, prob * 100))
-# 	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-# 	cv2.putText(image, label, (x - 10, y - 10),
-# 		cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
-# 	# show the image
-# 	cv2.imshow("Image", image)
-	# cv2.waitKey(0)
+# 	result += label
+# print('result: {}'.format(result))
+# loop over the predictions and bounding box locations together
+for (pred, (x, y, w, h)) in zip(preds, boxes):
+	# find the index of the label with the largest corresponding
+	# probability, then extract the probability and label
+	i = np.argmax(pred)
+	prob = pred[i]
+	label = labelNames[i]
+	# draw the prediction on the image
+	print("[INFO] {} - {:.2f}%".format(label, prob * 100))
+	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	cv2.putText(image, label, (x - 10, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
+	# show the image
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
